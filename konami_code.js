3e9a1840792d870665395d21dacdab39e6e67bfd
Ryan Namgung <ryannamgung@gmail.com>
2020-11-07 02:51:48 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+
 function init() {
-  // your code here
+  var num = 0;
+  function callback(e)
+  {
+    const key = e.key
+    if(key == codes[num])
+    {
+      num += 1;
+      if(num == codes.length)
+      {
+        window.alert('Hurray')
+        num = 0;
+      }
+    }else{
+      num = 0;
+    }
+  }
+
+  document.body.addEventListener("keydown", callback)
 }
